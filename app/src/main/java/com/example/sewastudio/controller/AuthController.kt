da4b82a20ed910893bf2afac01b5c753476e7bf0
Royhan Antariksa <103055171+royhanantariksaaa@users.noreply.github.com>
2024-01-01 13:59:17 +0700

    Revert "Moving update from other branch to main branch"
    This reverts commit fac206e4017c6f425cb7acc78c510d7193d097aa.

--- app/src/main/java/com/example/sewastudio/controller/AuthController.kt
+++ app/src/main/java/com/example/sewastudio/controller/AuthController.kt
@@ -88,41 +88,12 @@ class AuthController {
                 }
             })
         }
-
-        fun registerKaryawan(email : String, username : String, password: String, navController: NavController, prefMan: PreferencesManager,  callback: (Auth?) -> Unit) {
-            authService.register(RegisterData(email, username, password, UserRole.Karyawan)).enqueue(object : Callback<Auth> {
-                override fun onResponse(call: Call<Auth>, response: Response<Auth>): Unit =
-                    if (response.isSuccessful) {
-                        val respBody = response.body()!!
-                        val jwt = respBody.jwt
-                        val respUser = respBody.user!!
-                        val userID = respUser.id.toString()
-                        val role = respUser.status
-                        prefMan.saveData("jwt", jwt)
-                        prefMan.saveData("userID", userID)
-                        prefMan.saveData("username", username)
-                        prefMan.saveData("password", password)
-                        println(role)
-                        println("Succesful register")
-                        GoTo("pemilikhomepage", navController, prefMan)
-                    } else {
-                        println("Unsuccesful register")
-                        navController.navigate("auth-page")
-                        callback(null)
-                    }
-
-                override fun onFailure(call: Call<Auth>, t: Throwable) {
-//                    println(t)
-                    callback(null)
-                }
-            })
-        }
         fun logout (navController: NavController, prefMan: PreferencesManager) {
-            navController.navigate("auth-page")
             prefMan.saveData("jwt","")
             prefMan.saveData("userID","")
             prefMan.saveData("username","")
             prefMan.saveData("password","")
+            GoTo("auth-page", navController, prefMan)
         }
     }
 }
